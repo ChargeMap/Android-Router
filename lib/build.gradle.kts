10e0b2d56e7f885fbc5e830f9046de25d21a6136
Raphaël BERTIN <r4phab@gmail.com>
2020-11-02 12:57:38 +0100

    0.0.6

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -85,7 +85,7 @@ bintray {
     pkg.apply {
         repo = libProperties.getProperty("lib.name")
         name = libProperties.getProperty("lib.name")
-        userOrg = "${libProperties.getProperty("lib.extension")}.${libProperties.getProperty("lib.group")}"
+        userOrg = libProperties.getProperty("lib.group")
         githubRepo = githubRepo
         vcsUrl = "https://github.com/${libProperties.getProperty("lib.group")}/${libProperties.getProperty("lib.name")}"
         description = libProperties.getProperty("lib.desc")
